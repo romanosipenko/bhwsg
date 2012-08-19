d49f89fb54e771aa67f769dcaad2829bd9d68a6f
Roman Osipenko <roman.osipenko@djangostars.com>
2012-08-19 16:39:36 +0300

    Moved static from assets to proper dir. Login page.

--- src/bhwsg/urls.py
+++ src/bhwsg/urls.py
@@ -1,5 +1,6 @@
 from django.conf.urls import patterns, include, url
 from django.contrib import admin
+from django.conf import settings
 
 from core.views import home, login_user, logout_user
 
@@ -13,3 +14,13 @@ urlpatterns = patterns('',
     url(r'^login/$', login_user, name="auth_login"),
     url(r'^logout/$', logout_user, name="auth_logout"),
 )
+
+if settings.DEBUG:
+    urlpatterns += patterns('',
+        url(r'^media/(?P<path>.*)$', 'django.views.static.serve', {
+            'document_root': settings.MEDIA_ROOT,
+        }),
+        url(r'^static/(?P<path>.*)$', 'django.views.static.serve', {
+            'document_root': settings.STATIC_ROOT,
+        }),
+    )
