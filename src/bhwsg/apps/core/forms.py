c9e72fa4739853d2eda9875cff1159fd2bcb891b
Vasyl Stanislavchuk <vasyl.stanislavchuk@djangostars.com>
2012-08-19 14:23:13 +0300

    Added ability to log in by email

---
+++ src/bhwsg/apps/core/forms.py
@@ -0,0 +1,35 @@
+from django import forms
+from django.contrib.auth import authenticate
+from django.contrib.auth.models import User
+
+
+def validateEmail(email):
+    from django.core.validators import validate_email
+    from django.core.exceptions import ValidationError
+    try:
+        validate_email(email)
+        return True
+    except ValidationError:
+        return False
+
+
+class LoginForm(forms.Form):
+    username = forms.CharField(required=True)
+    password = forms.CharField(required=True, widget=forms.PasswordInput)
+
+    def clean_username(self):
+        username = self.cleaned_data['username']
+        if '@' in username:
+            if validateEmail(username) and User.objects.filter(email=username).exists():
+                    self.user = User.objects.get(email=username)
+            else:
+                raise forms.ValidationError("Enter valid email/username or password.")
+        else:
+            if User.objects.filter(username=username).exists():
+                self.user = User.objects.get(username=username)
+            else:
+                raise forms.ValidationError("Enter valid email/username or password.")
+        return username
+
+    def authenticate(self):
+        return authenticate(username=self.user.username, password=self.cleaned_data['password'])
--- src/bhwsg/apps/core/views.py
+++ src/bhwsg/apps/core/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, redirect
-from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth import login, logout
 
+from forms import LoginForm
 from inbox.forms import InboxCreateForm
 
 
@@ -13,14 +14,13 @@ def home(request):
 
 
 def login_user(request):
-    if request.method == "POST":
-        username = request.POST.get('username')
-        password = request.POST.get('password')
-        user = authenticate(username=username, password=password)
+    form = LoginForm(request.POST or None)
+    if request.method == "POST" and form.is_valid():
+        user = form.authenticate()
         if user is not None:
             login(request, user)
             return redirect('home')
-    return render(request, 'core/auth.html')
+    return render(request, 'core/auth.html', {"form": form})
 
 
 def logout_user(request):
